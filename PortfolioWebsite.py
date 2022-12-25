38ff7de11bd2578d8184014e7b414b5bd03f3c92
KRISTIJAN KOCEV <kristijankocev1234@gmail.com>
2022-12-25 17:13:46 +0100

    First commit

---
+++ PortfolioWebsite.py
@@ -0,0 +1,21 @@
+#Let's try and make a portfolio website look nice
+
+#I will be using bootstrap again and start 
+# working from a template
+
+#Then finetune and create something of my own.
+
+#Use the design rules and tips from day 65
+#Change the fonts and colors from the template
+#Pick a nice color scheme and font styles
+#make the links work and add info to them as well
+
+#Personalize and fill up every part of the page
+#Use chatgpt and google for everything i feel i cant do
+
+#Try to find a way to use Javascript and learn more about it
+#See how other devs used JS for their personal websites
+
+#Check out the designs from 
+# https://www.hostinger.com/tutorials/web-developer-portfolio
+#and copy what looks cool
